f3d9f8b489a3d4bc5f0d43aa21ce035095258cce
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-02-26 08:47:18 +0530

    System Schedule bot is uploaded

---
+++ JavaScript/26-Feb-23_system_schedule.js
@@ -0,0 +1,18 @@
+/**
+ * Author :- Sandip Nandi
+ * Creation Date :- 26-Feb-23
+ * Address :- Bangalore, Karnataka
+ **/
+
+const fs = require('fs');
+
+const dir = fs.readdirSync('.');
+const files = dir.filter(f => fs.statSync(f).isFile());
+const now = new Date();
+const filename = 'summary.log';
+
+fs.appendFileSync(filename, now + '\n');
+files.forEach(file => fs.appendFileSync(filename, `\t${file}\n`));
+fs.appendFileSync(filename, '\n');
+
+console.log(`Wrote ${files.length} entries for ${now} to summary.log`);
