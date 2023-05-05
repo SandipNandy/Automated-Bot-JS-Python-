11e26a2e7784f19accf9c41abf791c1876773c10
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-05-05 16:26:43 +0530

    Update 26-Feb-23_pdf_data_extract.py

--- Python/26-Feb-23_pdf_data_extract.py
+++ Python/26-Feb-23_pdf_data_extract.py
@@ -3,7 +3,6 @@
 ##Creation Date :- 26- feb-23               #
 ##Address :- Bengalore, Karanataka          #
 ##PDF DATA EXTRACTOR                        #
-##                                          #
 #############################################
 
 import os
