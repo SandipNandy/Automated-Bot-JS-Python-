7f7a8d687d0db51a274296fc00bf7aa0f0fae077
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-06-28 09:00:54 +0530

    Update batch_renamer_25-feb-23.js

--- JavaScript/batch_renamer_25-feb-23.js
+++ JavaScript/batch_renamer_25-feb-23.js
@@ -4,10 +4,8 @@
  *  Address :- Bnagalore, Karnataka
  *  Batch Renamer
 *********/
-
 const fs = require('fs');
 const path = require('path');
-
 const type_filter = '.txt';
 const search_File = 'file';
 const replace_File = 'document';
