ae2fe0b5d77129f5e1bae2301a22b64102215118
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-30 21:51:15 +0530

    Update excel_extract-26-Feb-23.py

--- Python/excel_extract-26-Feb-23.py
+++ Python/excel_extract-26-Feb-23.py
@@ -4,10 +4,6 @@
 # Address :- Bengalore, Karanataka          #
 # Excel Extract                             #
 #############################################
-
-
-
-
 import os
 from openpyxl import load_workbook, Workbook
 from openpyxl.styles import Font
