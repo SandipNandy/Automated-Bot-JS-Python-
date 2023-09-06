851c6a08a561cc1c9b95a59d633fb4566d3dbc8c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-06 09:17:45 +0530

    Update 26-Feb-23_system_schedule.py

--- Python/26-Feb-23_system_schedule.py
+++ Python/26-Feb-23_system_schedule.py
@@ -6,10 +6,10 @@
 #############################################
 
 import os
-
 import datetime
 
 # get the content of the current directory
+
 dir_content = os.listdir(".")
 
 # create a new summary.log file to append the content to
