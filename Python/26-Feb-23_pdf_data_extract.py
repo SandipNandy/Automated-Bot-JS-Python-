ab80acbac97e1aed7af186ae05049f12d8a41b12
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-06 09:11:54 +0530

    Update 26-Feb-23_pdf_data_extract.py

--- Python/26-Feb-23_pdf_data_extract.py
+++ Python/26-Feb-23_pdf_data_extract.py
@@ -12,6 +12,7 @@ import PyPDF2
 ##set up the email search pattern
 
 email_pattern = r"([a-zA-Z][a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-z]{2,5})"
+
 pattern = re.compile(email_pattern)
 
 ##define the output file
