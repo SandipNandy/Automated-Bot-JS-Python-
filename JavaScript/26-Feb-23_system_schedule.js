66b82269edab19bade86c89eab2e9285a2251bb8
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-30 11:20:53 +0530

    Update 26-Feb-23_system_schedule.js

--- JavaScript/26-Feb-23_system_schedule.js
+++ JavaScript/26-Feb-23_system_schedule.js
@@ -13,6 +13,7 @@ const dir = fs.readdirSync('.');
 const files = dir.filter(f => fs.statSync(f).isFile());
 
 const now = new Date();
+
 const filename = 'summary.log';
 fs.appendFileSync(filename, now + '\n');
 files.forEach(file => fs.appendFileSync(filename, `\t${file}\n`));
