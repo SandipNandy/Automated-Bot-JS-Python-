1af5aa4b7e19d340b97b1e84422077552fda04de
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-29 08:35:20 +0530

    Update 26-Feb-23_cron_schedule.js

--- JavaScript/26-Feb-23_cron_schedule.js
+++ JavaScript/26-Feb-23_cron_schedule.js
@@ -11,7 +11,6 @@ const os = require('os');
 const path =  require('path');
 const fs = require('fs');
 const oldFilesFolderName = 'old_files';
-
 const downloadsFolder = path.join(os.homedir(), 'Downloads', 'test');
 
 const oldFilesFolderPath = path.join(downloadsFolder, oldFilesFolderName);
