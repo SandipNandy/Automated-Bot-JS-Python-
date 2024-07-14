99497277d8e8214e384cfbdd1915ccf08d9568d9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-07-14 22:42:26 +0530

    Update 26-Feb-23_system_schedule.js

--- JavaScript/26-Feb-23_system_schedule.js
+++ JavaScript/26-Feb-23_system_schedule.js
@@ -17,4 +17,5 @@ fs.appendFileSync(filename, now + '\n');
 files.forEach(file => fs.appendFileSync(filename, `\t${file}\n`));
 
 fs.appendFileSync(filename, '\n');
+
 console.log(`Wrote ${files.length} entries for ${now} to summary.log`);
