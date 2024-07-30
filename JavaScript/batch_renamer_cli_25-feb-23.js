1d65303b9795f27400af8bde00922299671e6ed7
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-07-31 01:38:16 +0530

    Update batch_renamer_cli_25-feb-23.js

--- JavaScript/batch_renamer_cli_25-feb-23.js
+++ JavaScript/batch_renamer_cli_25-feb-23.js
@@ -34,6 +34,7 @@ console.log(args);
 
 // rename path to dirPath because of naming conflict with the path library
 const { filetype, search, replace, path: dirPath } = args;
+
 // filter the current directory for files
 const dir = fs.readdirSync(dirPath);
 const files = dir.filter(f => fs.statSync(f).isFile());
