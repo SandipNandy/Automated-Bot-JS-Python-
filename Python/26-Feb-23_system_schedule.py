1f0a536364b67e85c0f749a0e0c5855e9a0f93b2
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-05 08:10:53 +0530

    Update 26-Feb-23_system_schedule.py

--- Python/26-Feb-23_system_schedule.py
+++ Python/26-Feb-23_system_schedule.py
@@ -20,7 +20,6 @@ with open("summary.log", "a") as summary:
 
     # get the current time stamp and write a header for the current execution
     current_timestamp = datetime.datetime.now()
-    
     summary.write(f"{current_timestamp}\n")
 
     # write each entity of the folder content into the summary.log file
