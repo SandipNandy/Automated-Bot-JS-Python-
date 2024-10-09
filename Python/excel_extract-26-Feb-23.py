63866908370a3d40fab221931b525c97067dde31
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-09 09:52:07 +0530

    Update excel_extract-26-Feb-23.py

--- Python/excel_extract-26-Feb-23.py
+++ Python/excel_extract-26-Feb-23.py
@@ -13,7 +13,6 @@ from openpyxl.styles import Font
 ## mapping from index to letter
 letters = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", 
-    
     "J", "K", "L", "M", "N", "O", "P", "Q", "R", 
     
     "S", "T", "U", "V", "W", "X", "Y", "Z"
