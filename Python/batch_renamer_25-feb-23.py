b98b39fc042f993cd7b6f2ae20d6fa2984749051
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-14 20:38:41 +0530

    Update batch_renamer_25-feb-23.py

--- Python/batch_renamer_25-feb-23.py
+++ Python/batch_renamer_25-feb-23.py
@@ -13,7 +13,6 @@ replaceFile = "file"
 type_filter = ".py"
 
 ## get all files from current directory
-
 dir_contents = os.listdir('.')
 
 docs = [doc for doc in dir_contents if os.path.isfile(doc)]
